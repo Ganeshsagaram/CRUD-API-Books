d00b7d6df79cca080b77e773ecccbf19d6ed00cd
GANESH SAGARAM <ganeshsai1504@gmail.com>
2025-02-16 21:10:29 +0530

    changed the URI, now connected mongoDB atlas

--- books/mongoConnect.js
+++ books/mongoConnect.js
@@ -3,7 +3,7 @@ import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 dotenv.config();
 export async function dbConnection() {
-    const uri=process.env.URL;
+    const uri=process.env.ATLAS_URI;
     const client=new MongoClient(uri);
       const connection=await client.connect();
       return connection;
